82259e233963c97e9dedb6190aa0f86cd25fec4f
BestJaxX <xjl1729803140@gmail.com>
2022-07-08 16:35:27 +0800

    inittwo

--- src/main/java/test_git/HelloWorld.java
+++ src/main/java/test_git/HelloWorld.java
@@ -2,6 +2,6 @@ package test_git;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("hello world!呀");
+		System.out.println("hello world!呀,hellow!");
 	}
 }
